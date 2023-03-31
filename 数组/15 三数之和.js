9049b16e05e9e61f3e4b49e6c42bcf63520486db
maxence <mabiao811@foxmail.com>
2023-03-31 21:54:32 +0800

    three sum

--- 数组/15 三数之和.js
+++ 数组/15 三数之和.js
@@ -4,33 +4,85 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-	const res = []
-	const n = nums.length
-	if (!nums || n < 3) return res
-	// 双指针，要排序
-	nums.sort((a, b) => a - b)
-	for (let i = 0; i < n; i++) {
-		if (nums[i] > 0) break
-		// 去重1
-		if (i > 0 && nums[i] === nums[i - 1]) continue
-		let l = i + 1,
-			r = n - 1
-		while (l < r) {
-			const sum = nums[i] + nums[l] + nums[r]
-			if (sum === 0) {
-				res.push(Array(nums[i], nums[r], nums[l]))
-				// -3 -2 -1 -1 -1 0 1 2 2 3 3 5
-				// 直到走到不相同的数，再进行
-				while (l < r && nums[l] === nums[l + 1]) l++
-				while (l < r && nums[r] === nums[r - 1]) r--
-				r--
-				l++
-			} else if (sum > 0) {
-				r--
-			} else {
-				l++
-			}
-		}
-	}
-	return res
+    //create an empty array to store the results
+    const res = []
+
+    //create a variable to store the length of the nums array
+    const n = nums.length
+
+    //if there are less than 3 numbers in the array, return an empty array
+    if (!nums || n < 3) return res
+
+    //sort the array from smallest to largest
+    nums.sort((a, b) => a - b)
+
+    //iterate through the array
+    for (let i = 0; i < n; i++) {
+        //if the current number is greater than 0, break out of the loop
+        if (nums[i] > 0) break
+
+        //if the current number is the same as the previous number, continue to the next iteration
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+
+        //create variables to store the index of the next number and the last number in the array
+        let l = i + 1,
+            r = n - 1
+
+        //while the index of the next number is less than the index of the last number in the array
+        while (l < r) {
+            //create a variable to store the sum of the current number, the next number, and the last number in the array
+            const sum = nums[i] + nums[l] + nums[r]
+
+            //if the sum is equal to 0
+            if (sum === 0) {
+                //push the current number, the next number, and the last number into the results array
+                res.push(Array(nums[i], nums[r], nums[l]))
+                //while the index of the next number is less than the index of the last number in the array and the next number is the same as the next number after it
+                while (l < r && nums[l] === nums[l + 1]) l++ // skip same result
+                //while the index of the next number is less than the index of the last number in the array and the last number is the same as the last number before it
+                while (l < r && nums[r] === nums[r - 1]) r--
+                //decrement the index of the last number
+                r--
+                //increment the index of the next number
+                l++
+            } else if (sum > 0) {
+                //decrement the index of the last number
+                r--
+            } else {
+                //increment the index of the next number
+                l++
+            }
+        }
+    }
+    //return the results array
+    return res
+}
+
+var threeSum = function (nums) {
+    const res = []
+    const n = nums.length
+    if (!nums || n < 3) return res
+    nums.sort((a, b) => a - b)
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) break
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+        let l = i + 1,
+            r = n - 1
+        while (l < r) {
+            const sum = nums[i] + nums[l] + nums[r]
+            if (sum === 0) {
+                res.push(Array(nums[i], nums[r], nums[l]))
+                while (l < r && nums[l] === nums[l + 1]) l++ // skip same result
+                while (l < r && nums[r] === nums[r - 1]) r--
+                r--
+                l++
+            } else if (sum > 0) {
+                r--
+            } else {
+                l++
+            }
+        }
+    }
+    return res
 }
